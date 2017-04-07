d8c8f7aa2919acedaba80198e030f53e5abcfb01
Damon <Damon>
2017-04-07 00:44:52 -0400

    doing page 4

--- Source/BuckeyeStore.Core/Review.cs
+++ Source/BuckeyeStore.Core/Review.cs
@@ -2,5 +2,8 @@ namespace BuckeyeStore.Core
 {
     public class Review
     {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public Rating Rating { get; set; }
     }
 }

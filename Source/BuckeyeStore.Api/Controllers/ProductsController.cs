3d922059cf94e7bcd08638fa6f686ec3f59d176c
Damon <Damon>
2017-04-07 16:55:07 -0400

    doing exercise 2

---
+++ Source/BuckeyeStore.Api/Controllers/ProductsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BuckeyeStore.Core;
+using BuckeyeStore.Api.Data;
+
+namespace BuckeyeStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private BuckeyeStoreContext db;
+
+        public ProductsController(BuckeyeStoreContext context)
+        {
+            this.db = context;
+        }
+        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

73f024ddfa32cb55e2787a9e51e0b1c46fcfb640
Damon <Damon>
2017-04-07 00:12:26 -0400

    create new files

---
+++ Source/BuckeyeStore.Core/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BuckeyeStore.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Review Review { get; set; }
+        public AggregateRating AggregateRating { get; set; }
+        public List<Offer> Offers { get; set; }
+        public string Sku { get; set; }
+    }
+}
